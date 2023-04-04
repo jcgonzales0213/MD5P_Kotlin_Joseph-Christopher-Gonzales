76d311a8874b70cfc18d7b510602c5652467853f
Joseph <jcgonzales0213@gmail.com>
2023-04-04 20:36:42 +0800

    modified Joseph Christopher Samonte Gonzales

--- src/main/kotlin/KotlinProgram1.kt
+++ src/main/kotlin/KotlinProgram1.kt
@@ -1,4 +1,20 @@
 fun main(){
     print("Hello World!")
     print("Hi Universe!")
+    print("Joseph Christopher Samonte Gonzales")
 }
+/*
+Add print("Hi Universe")
+Git add .\src\main\kotlin\KotlinProgram1.kt
+git commit -m"Modified KotlinProgram1.kt"
+ */
+/*
+Add print("Full name")
+Git add .\src\main\kotlin\KotlinProgram1.kt
+git commit -m"Modified KotlinProgram1.kt"
+ */
+/*
+Add print("Address")
+Git add .\src\main\kotlin\KotlinProgram1.kt
+git commit -m"Modified KotlinProgram1.kt"
+ */
