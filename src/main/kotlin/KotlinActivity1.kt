d4f28961b3a365c0a385492025416cfc4f598519
Joseph Christopher Gonzales <jc@JCs-MacBook-Pro.local>
2023-04-15 18:56:26 +0800

    Kotlin Activity1

--- src/main/kotlin/KotlinActivity1.kt
+++ src/main/kotlin/KotlinActivity1.kt
@@ -2,54 +2,53 @@ fun main(){
     var position:String = "Mobile App Developer"
     val salary:Double = 35000.0
     val availability:String = "Anytime"
-    var lastname:String = "Dela Cruz"
-    var firstname:String = "San"
-    val middlename:String = "Juan"
-    var address:String = "Manila Polo Club Dr Forbes Park Vilage"
+    val lastName:String = "Dela Cruz"
+    val firstName:String = "San"
+    val middleName:String = "Juan"
+    var address:String = "Manila Polo Club Dr Forbes Park Village"
     var city:String = "Makati City"
-    var state:String = "Metro Manila"
+    val state:String = "Metro Manila"
     var zip:Int = 1219
-    val homephone:Int = 89111111
-    var cellphone:Long = 9626721455
+    val homePhone:Int = 89111111
+    var cellPhone:Long = 9626721455
     val email:String = "sjdelacruz@gmail.com"
-    var SSS:Int = 662348931
+    val sss:Int = 662348931
     val no:String = "No"
     val yes:String = "Yes"
-    val schoolname:String = "Colegio de San Juan de Letran"
-    val yearsattended:String = "2010 to 2014"
+    val schoolName:String = "Colegio de San Juan de Letran"
+    val yearsAttended:String = "2010 to 2014"
     val location:String = "Manila City"
     val degree:String = "Bachelor of Science Business Administration"
     val major:String = "Financial Management"
-    val othertraining:String = "NA"
+    val otherTraining:String = "NA"
     val employer:String = "ABC Company Inc."
-    val dateemployed:String = "2021 to 2023"
-    val workphone:Int = 88763156
-    val payrate:Double = 30000.0
+    val dateEmployed:String = "2021 to 2023"
+    val workPhone:Int = 88763156
+    val payRate:Double = 30000.0
     val duties:String = "Bank Reconciliation"
     val supervisor:String = "Jose Ona - Team Leader"
     val reason:String = "Health"
-    val contact:String = "Yes"
     var name: String = "Charles Lopez"
     var title: String = "Transitions Manager"
 
 
     println("*************************** Application for Employment ***************************")
-    println("Position you are applying for: $position                Desired Salary: $salary")
+    println("Position you are applying for: $position \tDesired Salary: $salary")
     println("Date Available for Work: $availability")
     println("*************************** PERSONAL INFORMATION ***************************")
-    println("Last Name: $lastname    First Name: $firstname    Middle Name: $middlename")
+    println("Last Name: $lastName    First Name: $firstName    Middle Name: $middleName")
     println("Address:  $address  City:$city   State:$state   Zip:$zip")
-    println("Home Phone: $homephone Cell Phone: 0$cellphone Email Address: $email")
-    println("Social Security Number: $SSS")
+    println("Home Phone: $homePhone Cell Phone: 0$cellPhone Email Address: $email")
+    println("Social Security Number: $sss")
     println("Are you a U.S. Citizen? $no")
     println("Have you ever been convicted of a felony? $no")
     println("If selected for employment are you willing to submit a pre-employment drug screening test? $no")
     println("*************************** EDUCATION ***************************")
-    println("School Name:$schoolname Years Attended: $yearsattended Location:$location  $degree $major ")
-    println("Other training, certifications or licenses held: $othertraining")
+    println("School Name:$schoolName Years Attended: $yearsAttended Location:$location  $degree $major ")
+    println("Other training, certifications or licenses held: $otherTraining")
     println("*************************** EMPLOYMENT ***************************")
-    println("Employer: $employer      Date Employed:$dateemployed")
-    println("Work Phone: $workphone    Pay Rate:$payrate")
+    println("Employer: $employer      Date Employed:$dateEmployed")
+    println("Work Phone: $workPhone    Pay Rate:$payRate")
     address = "Mother Ignacia Street"
     println("Address: $address")
     city = "Quezon City"
@@ -62,12 +61,12 @@ fun main(){
     println("Reason for leaving: $reason")
     println("May we contact them? $yes")
     println("*************************** REFERENCES ***************************")
-    cellphone = 9365112365
-    println("Name:$name    Title:$title    Company:$employer   Phone:0$cellphone")
+    cellPhone = 9365112365
+    println("Name:$name    Title:$title    Company:$employer   Phone:0$cellPhone")
     name = "April Bautista"
     title = "Team Leader"
-    cellphone = 9526443516
-    println("Name:$name    Title:$title    Company:$employer   Phone:0$cellphone")
+    cellPhone = 9526443516
+    println("Name:$name    Title:$title    Company:$employer   Phone:0$cellPhone")
     println("*************************** ACKNOWLEDGEMENT AND AUTHORIZATION ***************************")
     println("$yes ,I certify that all answers given herein are true and complete to the best of my knowledge.")
     println("$yes ,I authorize investigation of all statements contained in this application for employment as may be necessary in arriving at an employment decision")
